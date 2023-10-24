09044eaff1101e3e6ea58af54eab3f5a061d81f8
Tiago Leite <tiago.olileite@gmail.com>
2023-10-23 23:55:56 -0300

    feat(task-1): Configurando projeto com Rest

--- src/main/kotlin/br/com/fiap/lanchonetefilura/LanchonetefiluraApplication.kt
+++ src/main/kotlin/br/com/fiap/lanchonetefilura/LanchonetefiluraApplication.kt
@@ -2,8 +2,12 @@ package br.com.fiap.lanchonetefilura
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
+@ComponentScan
 class LanchonetefiluraApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/br/com/fiap/lanchonetefilura/domain/HelloController.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.lanchonetefilura.domain
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/hello")
+    fun hello(): String {
+        return "Hello World"
+    }
+}
