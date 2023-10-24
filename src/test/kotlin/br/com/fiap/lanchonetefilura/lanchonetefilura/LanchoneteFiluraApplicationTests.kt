2ac1ab6fd8275e28ac8125aa3a76bd6a65a5e719
Tiago Leite <tiago.olileite@gmail.com>
2023-10-23 22:46:18 -0300

    feat(task-1): Configurando Projeto Spring com Kotlin

---
+++ src/test/kotlin/br/com/fiap/lanchonetefilura/lanchonetefilura/LanchoneteFiluraApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.fiap.lanchonetefilura.lanchonetefilura
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LanchoneteFiluraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
