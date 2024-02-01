a32f8e37bec853fdb92ba0a1c70678d1dbd53813
Tiago Leite <tiago.olileite@gmail.com>
2024-02-01 03:38:26 -0300

    Ajuste arquitetura - Adapters

--- src/main/kotlin/br/com/fiap/lanchonetefilura/domain/adapter/CategoriaAdapter.kt
+++ src/main/kotlin/br/com/fiap/lanchonetefilura/domain/adapter/CategoriaAdapter.kt
@@ -1,14 +1,9 @@
 package br.com.fiap.lanchonetefilura.domain.adapter
 
-import br.com.fiap.lanchonetefilura.api.model.categoria.CategoriaResponse
-import br.com.fiap.lanchonetefilura.domain.dto.CategoriaDomainDTO
 import br.com.fiap.lanchonetefilura.domain.dto.impl.CategoriaDomainDTOImpl
 import br.com.fiap.lanchonetefilura.domain.entity.Categoria
 
 interface CategoriaAdapter {
-    fun adaptarCategoria(categoriaDTO: CategoriaDomainDTO?): CategoriaResponse
-
-    fun adaptarListaDeCategoria(categoriasDTO: List<CategoriaDomainDTO?>): List<CategoriaResponse>
 
     fun adaptarCategoriaParaCategoriaDto(categoria: Categoria): CategoriaDomainDTOImpl
 }
--- src/main/kotlin/br/com/fiap/lanchonetefilura/domain/adapter/impl/CategoriaAdapterImpl.kt
+++ src/main/kotlin/br/com/fiap/lanchonetefilura/domain/adapter/impl/CategoriaAdapterImpl.kt
@@ -1,32 +1,12 @@
 package br.com.fiap.lanchonetefilura.domain.adapter.impl
 
-import br.com.fiap.lanchonetefilura.api.model.categoria.CategoriaResponse
 import br.com.fiap.lanchonetefilura.domain.adapter.CategoriaAdapter
-import br.com.fiap.lanchonetefilura.domain.dto.CategoriaDomainDTO
 import br.com.fiap.lanchonetefilura.domain.dto.impl.CategoriaDomainDTOImpl
 import br.com.fiap.lanchonetefilura.domain.entity.Categoria
 import org.springframework.stereotype.Component
 
 @Component
 class CategoriaAdapterImpl : CategoriaAdapter {
-    override fun adaptarCategoria(categoriaDTO: CategoriaDomainDTO?): CategoriaResponse {
-        return CategoriaResponse(
-            id = categoriaDTO?.id,
-            descricao = categoriaDTO?.descricao
-        )
-    }
-
-    override fun adaptarListaDeCategoria(categoriasDTO: List<CategoriaDomainDTO?>): List<CategoriaResponse> {
-        val categoriasResponse: ArrayList<CategoriaResponse> = arrayListOf()
-
-        categoriasDTO.forEach {
-            categoriasResponse.add(CategoriaResponse(
-                id = it?.id,
-                descricao = it?.descricao))
-        }
-
-        return categoriasResponse.toList()
-    }
 
     override fun adaptarCategoriaParaCategoriaDto(categoria: Categoria): CategoriaDomainDTOImpl {
         return CategoriaDomainDTOImpl(descricao = categoria.descricao)
