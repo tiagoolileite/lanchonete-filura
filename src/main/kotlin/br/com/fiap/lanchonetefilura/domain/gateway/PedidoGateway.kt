f1ea8215faaf2b313da5d427780504220261c0a0
Tiago Leite <tiago.olileite@gmail.com>
2024-02-01 23:46:20 -0300

    Recap arq - Finalizando Refactor

--- src/main/kotlin/br/com/fiap/lanchonetefilura/domain/gateway/PedidoGateway.kt
+++ src/main/kotlin/br/com/fiap/lanchonetefilura/domain/gateway/PedidoGateway.kt
@@ -11,5 +11,5 @@ interface PedidoGateway {
 
     fun buscarPedidoPeloId(pedidoId: UUID): Optional<Pedido>
 
-    fun pagarPedido(pedidoDomainDTO: Pedido): Pedido
+    fun pagarPedido(pedido: Pedido): Pedido
 }
